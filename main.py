cf92e71502e641f8f9a920100b5dcdd9cc17baf0
sandeshveerani4 <sandeshveerani4@gmail.com>
2022-06-03 21:53:00 +0500

    fixed

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ def main():
     if choice == 1:
         takeOrder()
     elif choice == 2:
-        listOfOrders()
+        listOrder()
     elif choice == 3:
         addProduct()
     elif choice == 4:
-        listOfProducts()
+        listProduct()
     elif choice == 5:
         updateProduct()
     elif choice == 6:
